ad3908a52e18efcb73e9c595e4961a63f7566927
mohammadhshahwaz <shahwaz.m@iicl.in>
2025-07-24 12:22:42 +0530

    remove commented code.
    make a clean code

--- components/common/Loader/index.tsx
+++ components/common/Loader/index.tsx
@@ -1,12 +1,3 @@
-// const Loader = () => {
-//   return (
-//     <div className="flex  items-center justify-center dark:bg-black">
-//       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
-//     </div>
-//   );
-// };
-
-// export default Loader;
 
 
 
